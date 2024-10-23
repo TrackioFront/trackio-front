b5f33b61c381c9206470d627621084ad2f703104
Julian Muñoz <julianmunozrua@gmail.com>
2024-10-23 15:07:33 -0500

    feat reset password

---
+++ src/i18n.js
@@ -0,0 +1,37 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import translationES from './locales/es/translation.json';
+import translationEN from './locales/en/translation.json';
+
+const resources = {
+  es: {
+    translation: translationES
+  },
+  en: {
+    translation: translationEN
+  },
+};
+
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    supportedLngs: ['en', 'es'], // Idiomas soportados
+    fallbackLng: 'es', // Idioma por defecto
+    detection: {
+      order: ['path', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json', // Ruta de los archivos de traducción
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
