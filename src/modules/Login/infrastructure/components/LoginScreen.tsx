31183afd9bdf4997d7ebb4bf8a7e83f1a73a2c91
Julian Muñoz <julianmunozrua@gmail.com>
2024-10-23 10:38:27 -0500

    change register

--- src/modules/Login/infrastructure/components/LoginScreen.tsx
+++ src/modules/Login/infrastructure/components/LoginScreen.tsx
@@ -1,27 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Button from "../../../../components/Button/Button";
-import Input from "../../../../components/Input/Input";
 import "./LoginScreen.scss";
-import SocialMedia from "./SocialMedia/SocialMedia";
 import SignIn from "./SignIn/SignIn";
+import { PassRecovery } from "./PassRecovery/PassRecovery";
 
-interface SignUpFormInputs {
-  name: string;
-  email: string;
-  password: string;
-}
 
 const LoginScreen: React.FC = () => {
   const [isActive, setIsActive] = useState(false);
-  const [showSignUpPassword, setShowSignUpPassword] = useState(false);
 
-  const [formValues, setFormValues] = useState<SignUpFormInputs>({
-    name: "",
-    email: "",
-    password: "",
-  });
 
-  const [formErrors, setFormErrors] = useState<Partial<SignUpFormInputs>>({});
 
   const handleRegisterClick = () => {
     setIsActive(true);
@@ -31,30 +17,9 @@ const LoginScreen: React.FC = () => {
     setIsActive(false);
   };
 
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
 
-  const validateForm = () => {
-    let errors: Partial<SignUpFormInputs> = {};
-    if (!formValues.name) errors.name = "Name is required";
-    if (!formValues.email) errors.email = "Email is required";
-    if (!formValues.password) errors.password = "Password is required";
-    return errors;
-  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const errors = validateForm();
-    if (Object.keys(errors).length === 0) {
-      console.log("Sign Up Data:", formValues);
-    } else {
-      setFormErrors(errors);
-    }
-  };
+
 
   useEffect(() => {
     const registerBtn = document.getElementById("register");
@@ -71,41 +36,7 @@ const LoginScreen: React.FC = () => {
 
   return (
     <div className={`container ${isActive ? "active" : ""}`} id="container">
-      {/* Formulario de Sign Up */}
-      <div className="form-container sign-up">
-        <form onSubmit={handleSubmit}>
-          <SocialMedia title="Sign up" />
-          <span>or use your email for registration</span>
-          <Input
-            name="name"
-            type="text"
-            placeholder="Name"
-            value={formValues.name}
-            onChange={handleInputChange}
-            error={formErrors.name}
-          />
-          <Input
-            name="email"
-            type="email"
-            placeholder="Email"
-            value={formValues.email}
-            onChange={handleInputChange}
-            error={formErrors.email}
-          />
-          <div className="password-container">
-            <Input
-              name="password"
-              type={showSignUpPassword ? "text" : "password"}
-              placeholder="Password"
-              value={formValues.password}
-              onChange={handleInputChange}
-              error={formErrors.password}
-              showPasswordToggle={true}
-            />
-          </div>
-          <Button type="submit" text="Sign Up" />
-        </form>
-      </div>
+      <PassRecovery/>
       <SignIn />
     </div>
   );
---
+++ src/modules/Login/infrastructure/components/PassRecovery/PassRecovery.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Input  from "../../../../../components/Input/Input";
+import Button from "../../../../../components/Button/Button";
+
+
+interface SignUpFormInputs {
+    name: string;
+    email: string;
+    password: string;
+  }
+
+const PassRecovery: React.FC = () => {
+    const [formValues, setFormValues] = useState<SignUpFormInputs>({
+        name: "",
+        email: "",
+        password: "",
+      });
+    
+      const [formErrors, setFormErrors] = useState<Partial<SignUpFormInputs>>({});
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const errors = validateForm();
+        if (Object.keys(errors).length === 0) {
+          console.log("Sign Up Data:", formValues);
+        } else {
+          setFormErrors(errors);
+        }
+      };
+
+      const validateForm = () => {
+        let errors: Partial<SignUpFormInputs> = {};
+        if (!formValues.name) errors.name = "Name is required";
+        if (!formValues.email) errors.email = "Email is required";
+        if (!formValues.password) errors.password = "Password is required";
+        return errors;
+      };
+
+
+      const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+      ) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+      };
+    
+
+    return (
+        <div className="form-container sign-up">
+        <form onSubmit={handleSubmit}>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={formValues.email}
+            onChange={handleInputChange}
+            error={formErrors.email}
+          />
+          <Button type="submit" text="Recuperar contraseña" />
+        </form>
+      </div>
+    )
+}
+
+export { PassRecovery }
